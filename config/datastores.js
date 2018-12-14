99d3b735d776b6af4c029e3ccd173f9fac5cdd64
sunitha <sunitha.vibaker@gmail.com>
2018-12-14 09:23:40 +0530

    sails-deploy

--- config/datastores.js
+++ config/datastores.js
@@ -43,8 +43,8 @@ module.exports.datastores = {
   
 mongodb:{
   adapter: 'sails-mongo',
-  host: ' 127.0.0.1',
-  port:27017,
+  host: 'mongodb://sunitha:nivedan@1@ds115154.mlab.com',
+  port:15154,
   database: 'articlebase'
 },
 productionMongo:{
